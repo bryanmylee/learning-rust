5776d8e3b99a9f6611e82512756c6481b6d813b5
Bryan Lee <bryanmylee@gmail.com>
2022-10-26 00:20:48 +1100

    using cargo packages, match statement

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,13 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
 
@@ -17,5 +23,13 @@ fn main() {
         // Handle the Result type returned from `.read_line()`.
         .expect("Failed to read line");
 
-    println!("You guessed: {}", guess)
+    let guess: i32 = guess.trim().parse().expect("Please type a number!");
+
+    println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
