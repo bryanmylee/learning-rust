49397c064c43f2f6a2fefda5b671614334bca7b2
Bryan Lee <bryanmylee@gmail.com>
2023-08-13 15:42:41 +0800

    feat: impl traits

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,22 +1,31 @@
-fn largest<T: PartialOrd>(list: &[T]) -> &T {
-    let mut largest_item = &list[0];
-    for item in list.iter() {
-        if item > largest_item {
-            largest_item = item
-        }
-    }
-    largest_item
-}
-
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() {
-        x
-    } else {
-        y
-    }
-}
-
 fn main() {
+    generic_methods();
+}
+
+pub trait Summary {
+    // Specifying a trait method that has to be fulfilled by implementing structs.
+    fn summarize_author(&self) -> String;
+
+    // Default implementation for trait methods.
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+}
+
+fn generic_methods() {
     let list = vec![1, 2, 3, 4, 5, 9, 10, 4, 5];
     println!("Largest in {:?} is {}", list, largest(&list));
 
@@ -34,4 +43,22 @@ fn main() {
     // Fails to compile because `result` will receive the lifetime of `string2` and be invalid
     // after the previous scope ends.
     // println!("The longest string is {}", result);
+}
+
+fn largest<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest_item = &list[0];
+    for item in list.iter() {
+        if item > largest_item {
+            largest_item = item
+        }
+    }
+    largest_item
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
